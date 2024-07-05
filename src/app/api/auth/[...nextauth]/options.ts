817a97c8e29f45f8b8b141330745c5881c04ff40
Shubbu03 <thatcoderguyshubham@gmail.com>
2024-07-05 13:56:03 +0530

    feat: Add authentication options for credentials provider
    This commit adds the necessary code changes to enable authentication using the credentials provider in NextAuth. The `authOptions` object now includes a `providers` array with a `CredentialsProvider` configuration. This allows users to authenticate using their email and password credentials. The `authorize` function has been implemented to handle the authentication logic, including database queries and password validation.
    
    Based on the recent repository commits, this commit message follows the established convention of starting with a verb in the imperative form and providing a concise description of the changes made.

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,73 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import bcrypt from "bcryptjs";
+import { NextAuthOptions } from "next-auth";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials: any): Promise<any> {
+        await dbConnect();
+
+        try {
+          const user = await UserModel.findOne({
+            $or: [
+              { email: credentials.identifier },
+              { username: credentials.identifier },
+            ],
+          });
+
+          if (!user) {
+            throw new Error("No user found with this email!!");
+          }
+
+          const correctPassword = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+
+          if (correctPassword) {
+            return user;
+          } else {
+            throw new Error("Incorrect password!!");
+          }
+        } catch (error: any) {
+          throw new Error(error);
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async session({ session, token }) {
+      if (token) {
+        session.user._id = token._id;
+        session.user.monthlyExpense = token.monthlyExpense;
+        session.user.username = token.username;
+      }
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token._id = user._id?.toString();
+        token.monthlyExpense = user.monthlyExpense;
+        token.username = user.username;
+      }
+      return token;
+    },
+  },
+  pages: {
+    signIn: "/login",
+  },
+
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
