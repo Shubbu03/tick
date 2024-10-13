0ee5b8535d7696afa23f2f8bf9c7a33132b792b8
Shubbu03 <thatcoderguyshubham@gmail.com>
2024-10-13 17:49:19 +0530

    refactor: Update Navbar component styles and add logout functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,39 +6,39 @@ import { useSession, signOut } from "next-auth/react";
 import { User } from "next-auth";
 import { Button } from "./ui/button";
 import { Theme } from "./Theme";
+import { LogOut } from "lucide-react";
 
 const Navbar = () => {
   const { data: session } = useSession();
-
   const user = session?.user;
 
   return (
-    <nav className="p-4 md:p-6 shadow-md  bg-gray-900 text-white">
+    <nav className="bg-gradient-to-r from-purple-400 to-indigo-600 dark:from-gray-900 dark:to-gray-700 p-4 md:p-6 shadow-md">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-        <a href="#" className="text-xl font-bold mb-4 md:mb-0">
+        <Link href="/" className="text-2xl font-bold text-white mb-4 md:mb-0">
           Tick
-        </a>
+        </Link>
         {session ? (
-          <>
-            <span className="mr-4">
-              Welcome , {user?.username || user?.email}
+          <div className="flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-6">
+            <span className="text-white">
+              Welcome, {user?.username || user?.email}
             </span>
             <div className="flex items-center space-x-4">
               <Theme />
               <Button
                 onClick={() => signOut()}
-                className="w-full md:w-auto bg-slate-100 text-black"
-                variant="outline"
+                variant="secondary"
+                className="bg-white/10 backdrop-blur-md hover:bg-white/20 text-white"
               >
-                Logout
+                <LogOut className="mr-2 h-4 w-4" /> Logout
               </Button>
             </div>
-          </>
+          </div>
         ) : (
           <Link href="/login">
             <Button
-              className="w-full md:w-auto bg-slate-100 text-black"
-              variant={"outline"}
+              variant="secondary"
+              className="bg-white/10 backdrop-blur-md hover:bg-white/20 text-white"
             >
               Login
             </Button>
