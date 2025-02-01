05b7b721a7060c2a8ca4e3ef155123353ef9c8ef
Shubbu03 <thatcoderguyshubham@gmail.com>
2025-02-01 14:51:11 +0530

    Add PlanType and SubscriptionCategory enums to centralize subscription types

---
+++ src/lib/enums.ts
@@ -0,0 +1,18 @@
+export enum PlanType {
+  Monthly = "Monthly",
+  Quarterly = "Quarterly",
+  Half_Yearly = "Half_Yearly",
+  Yearly = "Yearly",
+}
+
+export enum SubscriptionCategory {
+  Music = "Music",
+  OTT = "OTT",
+  Fitness = "Fitness",
+  Education = "Education",
+  News = "News",
+  Gaming = "Gaming",
+  CloudStorage = "Cloud Storage",
+  Productivity = "Productivity",
+  ECommerce = "E-Commerce",
+}
--- src/model/User.ts
+++ src/model/User.ts
@@ -1,23 +1,5 @@
+import { PlanType, SubscriptionCategory } from "@/lib/enums";
 import mongoose, { Schema, Document } from "mongoose";
-
-export enum PlanType {
-  Monthly = "Monthly",
-  Quarterly = "Quarterly",
-  Half_Yearly = "Half_Yearly",
-  Yearly = "Yearly",
-}
-
-export enum SubscriptionCategory {
-  Music = "Music",
-  OTT = "OTT",
-  Fitness = "Fitness",
-  Education = "Education",
-  News = "News",
-  Gaming = "Gaming",
-  CloudStorage = "Cloud Storage",
-  Productivity = "Productivity",
-  ECommerce = "E-Commerce",
-}
 
 export interface Subscription extends Document {
   name: string;
